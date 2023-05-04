527b397ae0bf15e9ea719bbcc6964c20261a1ee5
samanthahaak <samantha.albano@gmail.com>
2023-05-04 11:25:48 -0300

    Solving challenges 6 and 7

---
+++ Mundo-01/desafio-6.py
@@ -0,0 +1,2 @@
+n6 = int(input('Digite um número: '))
+print(f'O dobro de {n6} é {n6*2}, \no triplo é {n6*3} \ne a raíz quadrada é {n6 ** (1/2):.2f}')
---
+++ Mundo-01/desafio-7.py
@@ -0,0 +1,4 @@
+g1 = float(input('Primeira nota: '))
+g2 = float(input('Segunda nota: '))
+m = (g1 + g2) / 2
+print(f'A média do aluno é {m:.1f}')
